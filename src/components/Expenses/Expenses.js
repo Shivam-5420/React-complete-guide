8482b64f0ed9ad6b34035b34334cd2efecf293cd
shivam.sharma1 <shivam.sharma1@emids.com>
2023-04-20 14:14:33 +0530

    added chart bars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
+import ExpensesChart from "./ExpensesChart";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 const  Expenses = (props)=> {
   const [filteredYear, setFilteredYear] = useState('2020');
 
@@ -11,19 +12,11 @@ const  Expenses = (props)=> {
   };
 
   const filteredExpenses= props.expenses.filter((expense)=> expense.date.getFullYear() === +filteredYear)
-  //const expenses=props.expenses;
-
- let expensesContent = <p>No Expenses till now.</p>
-
- if(filteredExpenses.length> 0){
-  expensesContent = filteredExpenses.map(expense => {
-    return <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
-  })
- }
 
   return (<Card className='expenses'>
   <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-  {expensesContent}
+  <ExpensesChart expenses={filteredExpenses}/>
+  <ExpensesList items={filteredExpenses}/>
   </Card>);
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props)=>{
+  const [isEditing, setIsEditing] = useState(false);
 
   const saveExpenseDataHandler= (enteredExpenseData) => {
     const expenseData = {
@@ -11,11 +12,21 @@ const NewExpense = (props)=>{
     }
     console.log(expenseData);
     props.onAddExpense(expenseData);
+    disableEditing();
+  }
+
+  const enableEditing= () => {
+    setIsEditing(true);
+  }
+
+  const disableEditing= () => {
+    setIsEditing(false);
   }
 
   return (
   <div className='new-expense'>
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    {!isEditing && <button onClick={enableEditing}>Add new Expense</button>}
+    {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={disableEditing} />}
   </div>
   );
 }
